8bf424fad70172600a3340475a91ef020baa8901
jotavit0r <jvitor2613@gmail.com>
2025-12-17 06:05:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,24 +6,25 @@ let leaveTimeout;
 
 links.forEach(link => {
   link.addEventListener('mouseenter', () => {
-    // cancela qualquer saída pendente
+    
     clearTimeout(leaveTimeout);
 
-    // aguarda um pequeno tempo antes de ativar o hover
+    
     enterTimeout = setTimeout(() => {
       links.forEach(l => l.classList.remove('ativo'));
       link.classList.add('ativo');
-    }, 150); // tempo de delay ao entrar (ms)
+    }, 150); 
   });
 
   link.addEventListener('mouseleave', () => {
-    // cancela o tempo de entrada se o mouse sair antes
+    
     clearTimeout(enterTimeout);
 
-    // aguarda um pequeno tempo antes de restaurar o link fixo
+    
     leaveTimeout = setTimeout(() => {
       link.classList.remove('ativo');
       homeLink.classList.add('ativo');
-    }, 300); // tempo de delay ao sair (ms)
+    }, 300); 
   });
 });
+
